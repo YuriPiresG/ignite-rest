14a890a5789b95eba7866e4ed44ce16e5b2b63b0
Yuri Pires <yuripiresbr@gmail.com>
2023-10-12 14:25:41 -0300

    refactor: changed variables to env

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { knex } from "./database";
 import { randomUUID } from "crypto";
+import { env } from "./env";
 
 const app = fastify();
 
@@ -12,7 +13,7 @@ app.get("/hello", async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("Server is running on port 3333");
