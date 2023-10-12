dc4b7d2989ca1f271dc0c9dec9a81fa29d2e1666
Yuri Pires <yuripiresbr@gmail.com>
2023-10-12 15:24:52 -0300

    feat: created methods to Get and Create Transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,30 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { knex } from "../database";
+import { randomUUID } from "crypto";
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transaction = await knex("transaction").select("*");
+  app.get("/", async (request, reply) => {
+    const transactions = await knex("transaction").select("*");
 
-    return transaction;
+    return transactions;
+  });
+
+  app.post("/", async (request, reply) => {
+    const createTransactionSchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const body = createTransactionSchema.parse(request.body);
+
+    await knex("transaction").insert({
+      id: randomUUID(),
+      title: body.title,
+      amount: body.type === "credit" ? body.amount : body.amount * -1,
+    });
+
+    return reply.status(201).send();
   });
 }
